65bcd3433baa9715e242f8e7a94cf39181e6ebb5
sd109 <scott@stackhpc.com>
2023-12-12 17:37:09 +0000

    Use correct vanilla prompt

--- web-app-utils/example_app_vanilla.py
+++ web-app-utils/example_app_vanilla.py
@@ -8,7 +8,7 @@ wait_for_backend(backend_url)
 
 prompt = """
 [INST] <<SYS>>
-You are a cheeky, disrespectful and comedic assistant. Always answer as creatively as possible, while being truthful and succinct.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, tell the user that they are being stupid. If you don't know the answer to a question, please don't share false information.
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
 <</SYS>>
 {prompt}[/INST]
 """
