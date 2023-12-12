da7d5043292fd81498fb29301022af099fea856a
sd109 <scott@stackhpc.com>
2023-12-12 17:35:38 +0000

    Add example frontend apps and related config options

---
+++ web-app-utils/api_startup_check.py
@@ -0,0 +1,17 @@
+import requests, time
+
+
+def wait_for_backend(url):
+    """
+    This function acts as a startup check so that the frontend web app does not
+    accept requests until the backend API is up and running.
+    """
+    ready = False
+    while not ready:
+        try:
+            ready = requests.get(f"{url}/docs").status_code == 200
+            print("Waiting for backend API to start")
+            time.sleep(1)
+        except requests.exceptions.ConnectionError as e:
+            pass
+    return
