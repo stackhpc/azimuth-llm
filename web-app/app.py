76b8d9d8d391a8ad0c4fe29a674b57ab0f173d1f
sd109 <scott@stackhpc.com>
2023-12-17 21:33:22 +0000

    Switch to yaml + pydantic for fronend app config

---
+++ web-app/app.py
@@ -0,0 +1,58 @@
+import requests, json, argparse, yaml
+import gradio as gr
+from api_startup_check import wait_for_backend
+from config import AppSettings
+
+settings = AppSettings.load("./settings.yml")
+
+backend_url = settings.backend_url
+wait_for_backend(backend_url)
+
+
+def inference(message, history):
+    context = ""
+    for user_input, system_response in history:
+        if settings.include_user_messages_in_context:
+            context += settings.user_context_template.format(user_input=user_input)
+        if settings.include_system_responses_in_context:
+            context += settings.system_context_template.format(
+                system_response=system_response
+            )
+    context += settings.user_context_template.format(user_input=message)
+
+    headers = {"User-Agent": "vLLM Client"}
+    payload = {
+        "prompt": settings.prompt_template.format(context=context),
+        "stream": True,
+        "max_tokens": settings.llm_max_tokens,
+        **settings.llm_params,
+    }
+    response = requests.post(
+        f"{backend_url}/generate", headers=headers, json=payload, stream=True
+    )
+
+    for chunk in response.iter_lines(
+        chunk_size=8192, decode_unicode=False, delimiter=b"\0"
+    ):
+        if chunk:
+            data = json.loads(chunk.decode("utf-8"))
+            output = data["text"][0]
+            # Manually trim the context from output
+            if "[/INST]" in output:
+                output = output.split("[/INST]")[-1]
+            yield output
+
+
+gr.ChatInterface(
+    inference,
+    chatbot=gr.Chatbot(
+        height=500,
+        show_copy_button=True,
+        # layout='panel',
+    ),
+    textbox=gr.Textbox(placeholder="Ask me anything...", container=False, scale=7),
+    title=settings.page_title,
+    retry_btn="Retry",
+    undo_btn="Undo",
+    clear_btn="Clear",
+).queue().launch(server_name="0.0.0.0")
