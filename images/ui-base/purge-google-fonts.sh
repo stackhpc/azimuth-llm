41d934f5ab833574967b2a236142a283b5f84bf3
sd109 <scott@stackhpc.com>
2024-05-01 08:38:08 +0100

    Pure additional CDN links

--- images/ui-base/purge-google-fonts.sh
+++ images/ui-base/purge-google-fonts.sh
@@ -4,9 +4,13 @@ SOURCE_CODE_PATH=/usr/local/lib/python3.11/site-packages/gradio/
 # Strip out preconnect directives from HTML <link> tags
 find $SOURCE_CODE_PATH -name "*.html" -type f -exec sed -i s!'rel="preconnect"'!!g {} \;
 
-# Replace links with something harmless but identifiable
-declare -a LINKS=("https://fonts.gstatic.com" "https://fonts.googleapis.com")
-REPLACEMENT="no-google-fonts-here"
+# Replace hard-coded links in HTML templates with something harmless but identifiable
+declare -a LINKS=(
+    "https://fonts.gstatic.com"
+    "https://fonts.googleapis.com"
+    "https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/.*/iframeResizer.contentWindow.min.js"
+)
+REPLACEMENT="no-GDPR-violations-here"
 for item in "${LINKS[@]}"; do
     find $SOURCE_CODE_PATH -name "*.html" -type f -exec sed -i s!$item!$REPLACEMENT!g {} \;
 done
