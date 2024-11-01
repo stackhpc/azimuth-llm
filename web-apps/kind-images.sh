69c05e4bf363f06ec8846e72c8f5c304a2db53a6
sd109 <sdavidson327@gmail.com>
2024-11-01 13:21:10 +0000

    Allow passing kind cluster name

--- web-apps/kind-images.sh
+++ web-apps/kind-images.sh
@@ -1,4 +1,4 @@
-# set -ex
+set -e
 
 source functions.sh
 
@@ -8,11 +8,13 @@ if [[ -z $1 ]]; then
 fi
 
 REMOTE_TAG=$1
+CLUSTER_NAME=${2:-kind}
+echo Kind cluster name: $CLUSTER_NAME
 KIND_TAG=local
 for image in $(find_images .); do
     full_name=ghcr.io/stackhpc/azimuth-llm-$image-ui
     echo $full_name
     docker pull $full_name:$REMOTE_TAG
     docker image tag $full_name:$REMOTE_TAG $full_name:$KIND_TAG
-    kind load docker-image $full_name:$KIND_TAG
+    kind load docker-image -n $CLUSTER_NAME $full_name:$KIND_TAG
 done
