c4126731bcd4a92223955a97d31e34b4a70bd44c
sd109 <sdavidson327@gmail.com>
2024-11-12 11:04:04 +0000

    Switch to pyproject.toml

--- web-apps/utils/setup.py
+++ web-apps/utils/setup.py
@@ -1,8 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-   name='web-app-utils',
-   version='0.0.1',
-   py_modules=["utils"],
-   requires=["pydantic"]
-)
+setup()
