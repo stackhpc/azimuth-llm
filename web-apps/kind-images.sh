50ef3c5031e3f789ea067340489f42efc77cf350
sd109 <sdavidson327@gmail.com>
2024-11-12 15:58:43 +0000

    Try using extra storage at /mnt

--- web-apps/kind-images.sh
+++ web-apps/kind-images.sh
@@ -9,15 +9,16 @@ fi
 
 # Clean up some storage capacity in CI runner
 if [[ $CI == "true" ]]; then
+    # Debug storage issues
+    set -x
+    docker system df
     docker system prune -af
+    docker system df
+    lsblk
+    df -h
+    set +x
 fi
 
-# Debug storage issues
-set -x
-docker system df
-lsblk
-df -h
-set +x
 
 REMOTE_TAG=$1
 CLUSTER_NAME=${2:-kind}
@@ -25,14 +26,17 @@ echo Kind cluster name: $CLUSTER_NAME
 KIND_TAG=local
 for image in $(find_images .); do
     full_name=ghcr.io/stackhpc/azimuth-llm-$image-ui
-    echo $full_name
+    echo $full_name:{$REMOTE_TAG,$KIND_TAG}
     docker pull $full_name:$REMOTE_TAG
     docker image tag $full_name:$REMOTE_TAG $full_name:$KIND_TAG
     # NOTE(scott): The 'load docker-image' command saves the
     # intermediate tar archive to /tmp which has limited space
     # inside a GH runner so do each step manually here instead.
     # kind load docker-image -n $CLUSTER_NAME $full_name:$KIND_TAG
-    docker image save -o ./image.tar $full_name:$KIND_TAG
-    kind load image-archive -n $CLUSTER_NAME ./image.tar
+    # Apparently there's a separate 75G disk at /mnt so try using it.
+    TAR_PATH=/mnt/image.tar
+    docker image save -o $TAR_PATH $full_name:$KIND_TAG
+    docker rm $full_name:{$REMOTE_TAG,$KIND_TAG}
+    kind load image-archive -n $CLUSTER_NAME $TAR_PATH
     rm image.tar
 done
