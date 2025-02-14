873558dfd19e75c88897357d27bf790c1021129f
sd109 <sdavidson327@gmail.com>
2025-02-14 19:30:26 +0000

    Wait for model readiness in test script

--- web-apps/chat/gradio-client-test.py
+++ web-apps/chat/gradio-client-test.py
@@ -1,7 +1,21 @@
 import sys
+import time
+
 from gradio_client import Client
 
 gradio_host = sys.argv[1]
-client = Client(gradio_host)
-result = client.predict("Hi", api_name="/chat")
-print(result)
+
+retries = 60
+for n in range(1, retries+1):
+    try:
+        client = Client(gradio_host)
+        result = client.predict("Hi", api_name="/chat")
+        print(result)
+        break
+    except Exception as err:
+        msg = f"Attempt {n} / {retries} encounter error: {err}"
+        if n < retries:
+            print(msg, "- waiting 10 seconds before retrying")
+            time.sleep(10)
+        else:
+            print(msg, "- no more retries left")
