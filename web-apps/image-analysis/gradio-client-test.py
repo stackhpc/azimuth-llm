56564cfa47fa671ab5e88cf37abf84e72b7106c0
sd109 <sdavidson327@gmail.com>
2025-02-14 22:44:57 +0000

    Try to speed up VLM test

--- web-apps/image-analysis/gradio-client-test.py
+++ web-apps/image-analysis/gradio-client-test.py
@@ -10,7 +10,7 @@ for n in range(1, retries+1):
     try:
         client = Client(gradio_host)
         result = client.predict(
-    		image_url="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*xDgN0L2RfkS8Kc7uTwsRqw.jpeg",
+            image_url="https://media.licdn.com/dms/image/v2/D4D0BAQHyxNra6_PoUQ/company-logo_200_200/company-logo_200_200/0/1704365018113/stackhpc_ltd_logo?e=1747872000&v=beta&t=Ed3-KZS-sHlg-ne1KC0YjI4Ez7yVvJzWr103nm5eVK0",
     		prompt="Hi",
     		api_name="/predict"
         )
