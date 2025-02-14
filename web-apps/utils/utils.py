88f957a461a5671ef95cc9bd32566f662d3d82ba
sd109 <sdavidson327@gmail.com>
2025-02-14 20:59:45 +0000

    Set in-cluster service addresses automatically

--- web-apps/utils/utils.py
+++ web-apps/utils/utils.py
@@ -43,28 +43,6 @@ class LLMParams(BaseModel):
     model_config = ConfigDict(extra="forbid")
 
 
-NAMESPACE_FILE_PATH = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
-
-
-def get_k8s_namespace():
-    try:
-        current_k8s_namespace = open(NAMESPACE_FILE_PATH).read()
-        return current_k8s_namespace
-    except FileNotFoundError:
-        return None
-
-
-def api_address_in_cluster():
-    k8s_ns = get_k8s_namespace()
-    if k8s_ns:
-        return f"http://llm-backend.{k8s_ns}.svc"
-    else:
-        log.warning(
-            "Failed to determine k8s namespace from %s - assuming non-kubernetes environment.",
-            NAMESPACE_FILE_PATH,
-        )
-
-
 # Method for loading settings from files
 def load_yaml(file_path: str) -> dict:
     with open(file_path, "r") as file:
@@ -95,12 +73,9 @@ def load_settings() -> dict:
             "Please check for typos"
         )
     settings = {**defaults, **overrides}
-    if "backend_url" not in settings or settings["backend_url"] == defaults["backend_url"]:
-        # Try to detect in-cluster address
-        in_cluster_backend = api_address_in_cluster()
-        if not in_cluster_backend:
-            raise Exception(
-                "Backend URL must be provided in settings when running outside of Kubernetes."
-            )
-        settings["backend_url"] = in_cluster_backend
+
+    if "backend_url" not in settings:
+        raise Exception(
+            "Backend URL must be provided in settings when running outside of Kubernetes."
+        )
     return settings
