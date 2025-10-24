8e35fde4b32dda4dadc7e1fade4e66ad3e68510d
Scott Davidson <sdavidson327@gmail.com>
2025-10-24 14:50:53 +0100

    Switch to non-containerised ollama in CI tests

--- web-apps/test-images.sh
+++ web-apps/test-images.sh
@@ -41,10 +41,12 @@ test() {
 
     if [[ -f $1/test.py ]]; then
 
-        DOCKER_NET_NAME=azimuth-llm-shared
-        if [[ ! $(docker network ls | grep $DOCKER_NET_NAME) ]]; then
-            docker network create $DOCKER_NET_NAME
-        fi
+        DOCKER_NET_NAME=host
+
+        # DOCKER_NET_NAME=azimuth-llm-shared
+        # if [[ ! $(docker network ls | grep $DOCKER_NET_NAME) ]]; then
+        #     docker network create $DOCKER_NET_NAME
+        # fi
 
         # Ensure app image is available
         IMAGE=$(image_name $1):$IMAGE_TAG
@@ -56,13 +58,15 @@ test() {
         fi
 
         # Ensure Ollama instance is available
-        if [[ $(curl -s localhost:11434) == "Ollama is running" ]]; then
-            log "Using existing ollama process running on localhost:11434"
-        else
-            log "Ollama process not running - starting containerised server"
-            docker run --rm --network $DOCKER_NET_NAME -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
-            sleep 3
-            docker exec ollama ollama pull smollm2:135m
+        if [[ $(curl -s localhost:11434) != "Ollama is running" ]]; then
+            log "Ollama not running on localhost:11434 - aborting test"
+            exit 1
+            # log "Using existing ollama process running on localhost:11434"
+        # else
+        #     log "Ollama process not running - starting containerised server"
+        #     docker run --rm --network $DOCKER_NET_NAME -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+        #     sleep 3
+        #     docker exec ollama ollama pull smollm2:135m
         fi
 
         log "Starting Gradio app container"
@@ -72,17 +76,23 @@ test() {
         sleep 10
 
         log "Running tests"
+        # docker run --network $DOCKER_NET_NAME --rm \
+        #     --name $1-test-suite \
+        #     -e GRADIO_URL=http://$1-app:7860 --entrypoint python \
+        #     $IMAGE \
+        #     test.py
         docker run --network $DOCKER_NET_NAME --rm \
             --name $1-test-suite \
-            -e GRADIO_URL=http://$1-app:7860 --entrypoint python \
+            --entrypoint python \
             $IMAGE \
             test.py
 
         log "Removing containers:"
-        docker rm -f ollama $1-app
+        # docker rm -f ollama $1-app
+        docker rm -f $1-app
 
-        log "Removing docker network:"
-        docker network rm $DOCKER_NET_NAME
+        # log "Removing docker network:"
+        # docker network rm $DOCKER_NET_NAME
 
         echo
         echo "----- Tests succeed -----"
